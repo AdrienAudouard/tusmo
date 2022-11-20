c430d67cd9999276e6112897303052f25f1a4032
Adrien AUDOUARD <adrien.audouard@akt.io>
2022-11-20 20:47:17 +0100

    setup analytics

--- src/components/game-board/GameBoard.tsx
+++ src/components/game-board/GameBoard.tsx
@@ -2,6 +2,7 @@ import GameGrid from '@components/game-grid/game-grid/GameGrid';
 import Keyboard from '@components/keyboard/keyboard/Keyboard';
 import GameLettersState from '@models/game-letter-state.model';
 import KeyboardLettersState from '@models/keyboard-letters-state.model';
+import './GameBoard.scss';
 
 interface Props {
   wordSize: number;
